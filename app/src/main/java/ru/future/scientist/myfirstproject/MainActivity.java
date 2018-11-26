fa042675108a80a042f2d37915f5e4c928709484
pirr <1234denis02@mail.ru>
2018-11-26 18:42:31 +0300

    add layout for item

--- app/src/main/java/ru/future/scientist/myfirstproject/MainActivity.java
+++ app/src/main/java/ru/future/scientist/myfirstproject/MainActivity.java
@@ -9,7 +9,6 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -26,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
             }
         });
     }
